750a53f0bf126a83720acf7dc0fa1013898f4ea7
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2025-01-07 10:23:15 +0500

    completed all api routes, nopw have to handle not found errors ansd responses

--- api/views.py
+++ api/views.py
@@ -2,16 +2,48 @@
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from api.models import Subject
-from  .serializers import SubjectSerializer
+from api.models import Answer, Question, Subject
+from  .serializers import AnswerSerializer, QuestionSerializer, SubjectSerializer
 
 
 @api_view(['GET'])
 
-def getSubjects(request):
+def getSubjects():
     
     subjects = Subject.objects.all()
     
     serializedSubjects = SubjectSerializer(subjects, many=True)
     
     return Response(serializedSubjects.data)
+
+@api_view(['GET'])
+
+def getQuestionsBySubjectId(request, subject_id):
+    
+    questions = Question.objects.filter(subject_id = subject_id)
+    serializedQuestions = QuestionSerializer(questions, many=True)
+    
+    return Response(serializedQuestions.data)
+    
+    
+@api_view(['GET'])
+
+def getQuestionById(request, subject_id, question_id):
+    
+    question = Question.objects.filter(subject_id = subject_id, id = question_id ).first()
+    
+    if not question: 
+        return Response({'message': 'question not found'}, status=404)
+    
+    serializedQuestion = QuestionSerializer(question)
+    
+    answers = Answer.objects.filter(question_id = question_id)
+    serializedAnswers = AnswerSerializer(answers, many=True)
+    
+    data = {
+        'question': serializedQuestion.data,
+        'answers': serializedAnswers.data
+    }
+
+    return Response(data)
+    
